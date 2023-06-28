74aad445a6a8f968339836be44ff45cc5bea8c32
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2023-06-28 15:43:20 +0900

    PUT, DELETE API 활용

---
+++ src/main/java/com/springboot/myproject/controller/DeleteController.java
@@ -0,0 +1,17 @@
+package com.springboot.myproject.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/delete-api")
+public class DeleteController {
+    @DeleteMapping("/{var}")
+    public String DeleteVariable(@PathVariable String var){
+        return var;
+    }
+
+    @DeleteMapping("/request1")
+    public String getRequestParam1(@RequestParam String email){
+        return "e-mail : " + email;
+    }
+}
---
+++ src/main/java/com/springboot/myproject/controller/PutController.java
@@ -0,0 +1,42 @@
+package com.springboot.myproject.controller;
+
+import com.springboot.myproject.dto.MemberDto;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+
+    @RequestMapping(value="/domain", method = RequestMethod.PUT)
+    public String postExample(){
+        return "PUT API";
+    }
+
+    @PutMapping("/member")
+    public String postMember(@RequestBody Map<String,Object> putData){
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map->{
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PutMapping("member2")
+    public MemberDto postMemberDto(@RequestBody MemberDto memberDto){
+        return memberDto;
+    }
+
+    //ResponseEntity를 적용한 예
+    @PutMapping("/member3")
+    public ResponseEntity<MemberDto> putMemberDto3(@RequestBody MemberDto memberDto){
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(memberDto);
+    }
+
+}
