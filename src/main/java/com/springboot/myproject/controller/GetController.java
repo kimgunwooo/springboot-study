b439da0d9130ac16ed4f5d7f056b044d823fa4a5
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2023-06-28 16:00:45 +0900

    Longback 적용

--- src/main/java/com/springboot/myproject/controller/GetController.java
+++ src/main/java/com/springboot/myproject/controller/GetController.java
@@ -1,6 +1,8 @@
 package com.springboot.myproject.controller;
 
 import com.springboot.myproject.dto.MemberDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
@@ -8,21 +10,27 @@ import java.util.Map;
 @RequestMapping("/api/v1/get-api")
 public class GetController {
 
+    //Logger 적용
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
+
     //@RequestMapping 사용
     @RequestMapping(value="/hello", method = RequestMethod.GET)
     public String hello(){
+        LOGGER.info("getHello 메서드 호출");
         return "Hello world";
     }
 
     //매개 변수 없는 Get 메서드
     @GetMapping("/name")
     public String getName(){
+        LOGGER.info("getName 메서드 호출");
         return "kimgunwooo";
     }
 
     //@PathVariable 사용
     @GetMapping("/variable1/{variable}")
     public String getVariable1(@PathVariable String variable){//@PathVariable("variable") String var
+        LOGGER.info("@PathVariable을 통해 들어온 값 : {}",variable);
         return variable;
     }
 
