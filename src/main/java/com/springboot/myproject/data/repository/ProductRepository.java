9dd42b0810efb51bcd0c70649b07c96eb44edd3c
김건우 <kwforu123@naver.com>
2023-07-23 01:57:10 +0900

    Query Annotation 적용

--- src/main/java/com/springboot/myproject/data/repository/ProductRepository.java
+++ src/main/java/com/springboot/myproject/data/repository/ProductRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Objects;
 
 public interface ProductRepository extends JpaRepository<Product,Long>{
     // Asc : 오름차순, Desc : 내림차순
@@ -15,4 +18,10 @@ public interface ProductRepository extends JpaRepository<Product,Long>{
 
     //Paging
     Page<Product> findByName(String name, Pageable pageable);
+
+    @Query("SELECT p FROM Product AS p WHERE p.name= :name")
+    List<Product> findByNameParam(@Param("name") String name);
+
+    @Query("SELECT p.name, p.price, p.stock FROM Product p WHERE p.name = :name")
+    List<Object[]> findByNameParam2(@Param("name") String name);
 }
