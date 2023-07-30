eb116f3ce873da48429508148e03086098fb686b
김건우 <96743351+kimgunwooo@users.noreply.github.com>
2023-07-30 21:51:47 +0900

    유효성 검사

---
+++ src/main/java/com/springboot/myproject/controller/ValidationController.java
@@ -0,0 +1,26 @@
+package com.springboot.myproject.controller;
+
+import com.springboot.myproject.data.dto.ValidRequestDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/validation")
+public class ValidationController {
+    private final Logger LOGGER = LoggerFactory.getLogger(ValidationController.class);
+
+    @PostMapping("/valid")
+    public ResponseEntity<String> checkValidationByValid(
+            @Valid @RequestBody ValidRequestDto validRequestDto){
+        LOGGER.info(validRequestDto.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(validRequestDto.toString());
+    }
+}
